6ec0996ab84d8b730c7f1f7a672c0eb4a5d9085c
tkpb <thiagokalleb@gmail.com>
2015-08-24 00:02:35 -0300

    Adapt to Python 3.x

--- ForkingServer.py
+++ ForkingServer.py
@@ -1,20 +1,15 @@
-#!/usr/bin/env python
-# Python Network Programming Cookbook -- Chapter - 2
-# This program is optimized for Python 2.7.
-# It may run on any other version with/without modifications.
-# See more: http://docs.python.org/2/library/socketserver.html
 import os
 import socket
 import threading
-import SocketServer
+import socketserver
 
 SERVER_HOST = 'localhost'
-SERVER_PORT = 0  # tells the kernel to pick up a port dynamically
+SERVER_PORT = 9000
 BUF_SIZE = 1024
 ECHO_MSG = 'Hello echo server!'
 
 
-class ForkedClient():
+class ForkedClient:
     """ A client to test forking server"""
     def __init__(self, ip, port):
         # Create a socket
@@ -27,31 +22,31 @@ class ForkedClient():
 
         # Send the data to server
         current_process_id = os.getpid()
-        print 'PID %s Sending echo message to the server : "%s"' % (current_process_id, ECHO_MSG)
+        print('PID %s Sending echo message to the server : "%s"' % (current_process_id, ECHO_MSG))
         sent_data_length = self.sock.send(ECHO_MSG)
-        print "Sent: %d characters, so far..." % sent_data_length
+        print("Sent: %d characters, so far..." % sent_data_length)
 
         # Display server response
         response = self.sock.recv(BUF_SIZE)
-        print "PID %s received: %s" % (current_process_id, response[5:])
+        print("PID %s received: %s" % (current_process_id, response[5:]))
 
     def shutdown(self):
         """ Cleanup the client socket """
         self.sock.close()
 
 
-class ForkingServerRequestHandler(SocketServer.BaseRequestHandler):
+class ForkingServerRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
         # Send the echo back to the client
         data = self.request.recv(BUF_SIZE)
         current_process_id = os.getpid()
         response = '%s: %s' % (current_process_id, data)
-        print "Server sending response [current_process_id: data] = [%s]" % response
+        print("Server sending response [current_process_id: data] = [%s]" % response)
         self.request.send(response)
         return
 
 
-class ForkingServer(SocketServer.ForkingMixIn, SocketServer.TCPServer):
+class ForkingServer(socketserver.ForkingMixIn, socketserver.TCPServer):
     """Nothing to add here, inherited everything necessary from parents"""
     pass
 
@@ -63,7 +58,7 @@ def main():
         server_thread = threading.Thread(target=server.serve_forever)
         server_thread.setDaemon(True)  # don't hang on exit
         server_thread.start()
-        print 'Server loop running PID: %s' % os.getpid()
+        print('Server loop running PID: %s' % os.getpid())
 
         # Launch the client(s)
         client1 = ForkedClient(ip, port)
