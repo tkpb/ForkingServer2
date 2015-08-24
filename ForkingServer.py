068d7f86256a524716fb1a2ac1e32d525add08e0
tkpb <thiagokalleb@gmail.com>
2015-08-24 01:09:58 -0300

    Split server and client

--- ForkingServer.py
+++ ForkingServer.py
@@ -10,32 +10,6 @@ ECHO_MSG = 'Hello echo server!'
 DEFAULT_ENCODING = 'ascii'
 
 
-class ForkedClient:
-    """ A client to test forking server"""
-    def __init__(self, ip, port):
-        # Create a socket
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # Connect to the server
-        self.sock.connect((ip, port))
-
-    def run(self):
-        """ Client playing with the server"""
-
-        # Send the data to server
-        current_process_id = os.getpid()
-        print('PID %s Sending echo message to the server : "%s"' % (current_process_id, ECHO_MSG))
-        sent_data_length = self.sock.send(ECHO_MSG.encode(DEFAULT_ENCODING))
-        print("Sent: %d characters, so far..." % sent_data_length)
-
-        # Display server response
-        response = self.sock.recv(BUF_SIZE)
-        print("PID %s received: %s" % (current_process_id, response[5:]))
-
-    def shutdown(self):
-        """ Cleanup the client socket """
-        self.sock.close()
-
-
 class ForkingServerRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
         # Send the echo back to the client
@@ -61,16 +35,8 @@ def main():
         server_thread.start()
         print('Server loop running PID: %s' % os.getpid())
 
-        # Launch the client(s)
-        client1 = ForkedClient(ip, port)
-        client1.run()
-        client2 = ForkedClient(ip, port)
-        client2.run()
-
         # Clean them up
         server.shutdown()
-        client1.shutdown()
-        client2.shutdown()
         server.socket.close()
 
 if __name__ == '__main__':
